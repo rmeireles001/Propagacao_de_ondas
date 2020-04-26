18ba3c5bc5704eeea3014408b70b94286587652d
rmeireles001 <43682825+rmeireles001@users.noreply.github.com>
2020-04-26 15:17:54 -0300

    via Windows

--- manipulacao/sort.c
+++ manipulacao/sort.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define tam 100
+#define tam 1000
 
 void insertionSort(double arr[], int n){
 	int i, j, key;
@@ -18,6 +18,8 @@ void insertionSort(double arr[], int n){
 int main(){
 	double aco[tam], lj[tam];
 	int i;
+	double acopior=10e-10, acomelhor=10e10, ljpior=10e-10, ljmelhor=10e10;
+	int acomp=0, acopp=0, ljmp=0, ljpp=0;
 	FILE *f = fopen("tempos.txt", "r");
 	for(i=0; i<tam; i++){
 		fscanf(f, "%lf", aco[i]);
@@ -26,10 +28,30 @@ int main(){
 		fscanf(f, "%lf", lj[i]);
 	}
 	fclose(f);
+	for(i=0; i<tam; i++){
+		if(aco[i]>acopior){
+			acopior = aco[i];
+			acopp = i+1;
+		}
+		if(aco[i]<acomelhor){
+			acomelhor = aco[i];
+			acomp = i+1;
+		}
+		if(lj[i]>ljpior){
+			ljpior = lj[i];
+			ljpp = i+1;
+		}
+		if(lj[i]<ljmelhor){
+			ljmelhor = lj[i];
+			ljmp = i+1;
+		}
+	}
 	insertionSort(aco, tam);
 	insertionSort(lj, tam);
 	FILE *s = fopen("ord.txt", "w");
-	fprintf(s, "ANTCOLONY OPTIMIZATION\n\n");
+	fprintf(s, "ANTCOLONY OPTIMIZATION:\nPior execução: #%d\nTempo da pior execução: %lf\nMelhor execução: #%d\nTempo da melhor execução: %lf\n", acopp, acopior, acomp, acomelhor);
+	fprintf(s, "LUUS JAAKOLA:\nPior execução: #%d\nTempo da pior execução: %lf\nMelhor execução: #%d\nTempo da melhor execução: %lf\n", ljpp, ljpior, ljmp, ljmelhor);
+	fprintf(s, "\n\nANTCOLONY OPTIMIZATION\n\n");
 	for(i=0; i<tam; i++){
 		fprintf(s, "%lf\n", aco[i]);
 	}
