bd0500df2b88b92b928cd1d6ab5fed0c171f75c5
Rafael Meireles <raphaelmeirelles0066@gmail.com>
2020-08-19 17:46:10 -0300

    via Debian

--- parameters.h
+++ parameters.h
@@ -1,12 +1,12 @@
 #define ANTS 20
-#define ncmax 50
+#define ncmax 15
 #define runs 1
 #define gamma 0.1
 #define rho 0.1
 
 //Other constants
-#define LSANTS 25
-#define lsncmax  15
+#define LSANTS 15
+#define lsncmax  10
 #define lsnvalues 2
 #define lsrho 0.5
 #define tau0 1
@@ -14,4 +14,4 @@
 #define q0 0.0
 #define localupdate 1
 #define alpha 1
-#define localsearch 0
+#define localsearch 1
